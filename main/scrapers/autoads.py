677bbf5941a3b924e3eb81c74ffea57b95218fee
denisdbell@gmail.com <denis18jam@hotmail.com>
2024-08-10 18:07:31 -0500

    Add database saving functionality

--- main/scrapers/autoads.py
+++ main/scrapers/autoads.py
@@ -1,10 +1,12 @@
 
 import requests
 from bs4 import BeautifulSoup
+from Orm.orm import CarDatabase,CarListings
+import uuid
 
 # Define the base URL and the number of pages you want to scrape
 base_url = 'https://www.autoadsja.com/search.asp?SearchSB=5&page='
-num_pages = 200 # Replace with the actual number of pages
+num_pages = 20 # Replace with the actual number of pages
 
 
 
@@ -17,6 +19,8 @@ class car:
     self.price = None
 
 cars = []
+
+car_db = CarDatabase()
 
 # Loop through the paginated pages
 for page in range(1, num_pages + 1):
@@ -41,11 +45,11 @@ for page in range(1, num_pages + 1):
         # Extract make and model
         for listing in car_listings:          
            # Get thumbnail image
-            new_car = car()
+            new_car = CarListings()
             img_tag_src = listing.find('img')['src']
             if img_tag_src:
                img_url = img_tag_src.strip()
-               new_car.img_url = img_url
+               new_car.IMAGE_URL = img_url
 
             #Get make model and year
             description = listing.find('div', class_='description')
@@ -53,14 +57,15 @@ for page in range(1, num_pages + 1):
                 span_tag = description.find('span', class_='visible-xs')
                 if span_tag:
                     car_price = ''.join(filter(str.isdigit,span_tag.text.strip()))
-                    new_car.price = car_price
+                    new_car.PRICE = float(car_price.strip(' "')) 
                 h2_tag = description.find('h2')
                 if h2_tag:
                     car_info  = h2_tag.text.strip().split()
-                    new_car.year  = car_info[0]
-                    new_car.make  = car_info[1]
-                    new_car.model = car_info[2]
-            
+                    new_car.YEAR  = int(car_info[0].strip(' "')) 
+                    new_car.MAKE  = car_info[1]
+                    new_car.MODEL = car_info[2]
+            new_car.UUID = uuid.uuid4()
+            new_car.WEBSITE = "AUTOADS"
             cars.append(new_car)
                
     else:
@@ -68,5 +73,11 @@ for page in range(1, num_pages + 1):
 
 # Print extracted make and model
 for car in cars:
-    print(car.year, car.make, car.model, car.img_url, car.price)
+    try:
+      car_db.add_car_listing(car)
+      #print("Added successfully",car.YEAR, car.MAKE, car.MODEL, car.PRICE, car.IMAGE_URL)
+    except:
+      print("An exception occurred",car.YEAR, car.MAKE, car.MODEL, car.PRICE, car.IMAGE_URL)
+    
+
 
